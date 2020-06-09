d64c0d17dd451b571a87097f8f4165707181dcb8
Michaeldremy <michaeldremy@gmail.com>
2020-06-09 12:21:13 -0700

    added the ability to create a tweet

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,20 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+from .forms import TweetForm
 from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
 
 def tweet_list_view(request, *args, **kwargs):
     """
@@ -14,7 +24,7 @@ def tweet_list_view(request, *args, **kwargs):
     """
 
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 1235)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
