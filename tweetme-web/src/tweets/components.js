83b47af5191fef17db7a434b70ca6fd3a6403b99
Michaeldremy <michaeldremy@gmail.com>
2020-06-15 14:06:45 -0700

    Building the React Front and to communicate with the django rest framework back-end

---
+++ tweetme-web/src/tweets/components.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from 'react'
+
+import {loadTweets} from '../lookup'
+
+export function TweetsList(props) {
+
+    const [tweets, setTweets] = useState([])
+
+    useEffect(() => {
+        const myCallback = (response, status) => {
+        if (status === 200) {
+            setTweets(response)
+        } else {
+            alert("There was an error")
+        }
+    }
+    loadTweets(myCallback)
+    }, [])
+    return tweets.map((item, index) => {
+        return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
+    })
+}
+
+export function ActionBtn(props) {
+    const {tweet, action} = props
+    const className = props.className ? props.className : 'btn btn-primary btn-sm'
+    const actionDisplay = action.display ? action.display : 'Action'
+    let likes = tweet.likes
+    const handleClick = (event) => {
+        event.preventDefault()
+        if (action.type === 'like') {
+            console.log(tweet.likes+1)
+            likes = tweet.likes + 1
+        }
+    }
+    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
+    return <button className={className} onClick={handleClick}>{display}</button>
+}
+
+export function Tweet(props) {
+    const {tweet} = props
+    const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    return <div className={className}>
+    <p>{tweet.id} - {tweet.content}</p>
+    <div className="btn btn-group">
+        <ActionBtn tweet={tweet} action={{type: "like", display: "Likes"}}/>
+        <ActionBtn tweet={tweet} action={{type: "unlike", display: "Unlike"}}/>
+        <ActionBtn tweet={tweet} action={{type: "retweet", display: "Retweet"}}/>
+    </div>
+    </div>
+}
--- tweetme/settings.py
+++ tweetme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=+o0&(8*_1b*c@n$3_8ni$51c8-637(14a-y7+1$@zca+v0$(n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.cfe.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.cfe.sh', 'localhost']
 LOGIN_URL = "/login"
 MAX_TWEET_LENGTH = 240
 TWEET_ACTION_OPTIONS = ['like', 'unlike', 'retweet']
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third-party
     'rest_framework',
+    'corsheaders',
     # internal
     'tweets',
 ]
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True # any webstie has access to my API
+CORS_URLS_REGEX = r'^/api/.*$'
 
 DEFAULT_RENDERER_CLASSES = [
     'rest_framework.renderers.JSONRenderer',
