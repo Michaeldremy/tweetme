2f473ffc23e85dd9755bb6e6442f77ea1134e887
Michaeldremy <michaeldremy@gmail.com>
2020-06-19 15:11:56 -0700

    Added static files and ran a build for React also integrated React front end with django rest framework

--- tweetme-web/src/index.js
+++ tweetme-web/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {TweetComponent} from './tweets'
+import {TweetsComponent} from './tweets'
 import * as serviceWorker from './serviceWorker';
 
 const appEl = document.getElementById('root')
 if (appEl) {
   ReactDOM.render( <App />, appEl);
+}
+
+const tweetsEl = document.getElementById("tweetme2")
+
+if (tweetsEl) {
+  ReactDOM.render( <TweetsComponent />, tweetsEl);
 }
 
 // If you want your app to work offline and load faster, you can change
--- tweetme-web/src/tweets/components.js
+++ tweetme-web/src/tweets/components.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 
-import {loadTweets} from '../lookup'
+import {createTweet, loadTweets} from '../lookup'
 
 export function TweetsComponent(props) {
     const textAreaRef = React.createRef(0)
@@ -9,10 +9,13 @@ export function TweetsComponent(props) {
         event.preventDefault()
         const newVal = textAreaRef.current.value
         let tempNewTweets = [...newTweets]
-        tempNewTweets.unshift({
-            content: newVal,
-            likes: 0,
-            id: 1231
+        createTweet(newVal, (response, status) => {
+            if (status === 201) {
+                tempNewTweets.unshift(response)
+            } else {
+                console.log(response)
+                alert("An error occurred please try again")
+            }
         })
         setNewTweets(tempNewTweets)
         textAreaRef.current.value = ''
@@ -35,6 +38,7 @@ export function TweetsList(props) {
 
     const [tweetsInit, setTweetsInit] = useState([])
     const [tweets, setTweets] = useState([])
+    const [tweetsDidSet, setTweetsDidSet] = useState(false)
     useEffect(() => {
         const final = [...props.newTweets].concat(tweetsInit)
         if (final.length !== tweets.length) {
@@ -42,15 +46,18 @@ export function TweetsList(props) {
         }
     }, [props.newTweets, tweets, tweetsInit])
     useEffect(() => {
-        const myCallback = (response, status) => {
-        if (status === 200) {
-            setTweetsInit(response)
-        } else {
-            alert("There was an error")
+        if (tweetsDidSet === false) {
+            const myCallback = (response, status) => {
+                if (status === 200) {
+                    setTweetsInit(response)
+                    setTweetsDidSet(true)
+                } else {
+                    alert("There was an error")
+                }
+            }
+            loadTweets(myCallback)
         }
-    }
-    loadTweets(myCallback)
-    }, [])
+    }, [tweetsInit, tweetsDidSet, setTweetsDidSet])
     return tweets.map((item, index) => {
         return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
     })
