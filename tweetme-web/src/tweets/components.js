9001016cc9dc243f50e1a110ed3228f0cbfef1c3
Michaeldremy <michaeldremy@gmail.com>
2020-06-22 09:18:17 -0700

    Added Dev Authentication and fixed tweet creating issue on React side

--- tweetme-web/src/tweets/components.js
+++ tweetme-web/src/tweets/components.js
@@ -5,19 +5,22 @@ import {createTweet, loadTweets} from '../lookup'
 export function TweetsComponent(props) {
     const textAreaRef = React.createRef(0)
     const [newTweets, setNewTweets] = useState([])
+
+    const handleBackendUpdate = (response, status) => {
+        let tempNewTweets = [...newTweets]
+        if (status === 201) {
+            tempNewTweets.unshift(response)
+            setNewTweets(tempNewTweets)
+        } else {
+            console.log(response)
+            alert("An error occurred please try again")
+        }
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault()
         const newVal = textAreaRef.current.value
-        let tempNewTweets = [...newTweets]
-        createTweet(newVal, (response, status) => {
-            if (status === 201) {
-                tempNewTweets.unshift(response)
-            } else {
-                console.log(response)
-                alert("An error occurred please try again")
-            }
-        })
-        setNewTweets(tempNewTweets)
+        createTweet(newVal, handleBackendUpdate)
         textAreaRef.current.value = ''
     }
 
